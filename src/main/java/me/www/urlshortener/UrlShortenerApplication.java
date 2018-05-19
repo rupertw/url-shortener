68162332f21000d4508d3732f9c5fe0f33d69953
www <rupertwong@163.com>
2018-05-19 22:28:25 +0800

    project init

--- src/main/java/me/www/urlshortener/UrlShortenerApplication.java
+++ src/main/java/me/www/urlshortener/UrlShortenerApplication.java
@@ -3,6 +3,9 @@ package me.www.urlshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author www
+ */
 @SpringBootApplication
 public class UrlShortenerApplication {
 
