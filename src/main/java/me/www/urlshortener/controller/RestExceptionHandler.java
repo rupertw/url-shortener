77ba92c3d18108ff43044fc06ed42cb1b8302d73
www <rupertwong@163.com>
2018-07-22 18:40:20 +0800

    ExceptionHandler

--- src/main/java/me/www/urlshortener/controller/RestExceptionHandler.java
+++ src/main/java/me/www/urlshortener/controller/RestExceptionHandler.java
@@ -6,8 +6,12 @@ import me.www.urlshortener.rest.RestResult;
 import me.www.urlshortener.rest.RestResultGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * @author: www
@@ -15,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
  * @description: 异常处理
  */
 @ControllerAdvice(annotations = RestController.class)
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -30,6 +34,13 @@ public class RestExceptionHandler {
     private RestResult illegalParamsExceptionHandler(IllegalParamsException e) {
         logger.error("--------->请求参数不合法!", e);
         return RestResultGenerator.genErrorResult(RestErrorEnum.ILLEGAL_PARAMS);
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<Object> internalServerError(RuntimeException e, WebRequest request) {
+        logger.error(e.getMessage(), e);
+        return handleExceptionInternal(e, null,
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
 }
